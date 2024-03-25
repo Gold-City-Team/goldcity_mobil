31f87aea21ad23e4332b0977222af57a9db8aa18
Zafer Çetin <zaferkurumsal@gmail.com>
2024-03-25 16:46:15 +0300

    fast

--- lib/view/presentation/gold_map/widget/facilities_widget.dart
+++ lib/view/presentation/gold_map/widget/facilities_widget.dart
@@ -1,5 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:gap/gap.dart';
+import 'package:goldcity/util/constant/general_enum.dart';
 import 'package:goldcity/util/extension/design_extension.dart';
+import 'package:goldcity/util/extension/theme_extension.dart';
+import 'package:goldcity/view/widget/image/normal_network_image.dart';
+import 'package:goldcity/view/widget/text/label_text.dart';
 
 class FacilitiesWidget extends StatelessWidget {
   const FacilitiesWidget({super.key});
@@ -11,9 +18,42 @@ class FacilitiesWidget extends StatelessWidget {
       width: 300,
       decoration: BoxDecoration(
         borderRadius: context.largeRadius,
-        color: Colors.red,
+        border: Border.all(color: context.toColor(APPLICATION_COLOR.GOLD)),
+        color: context.toColor(APPLICATION_COLOR.BACKGROUND_COLOR),
       ),
       margin: context.midSpacerOnlyHorizontal,
+      child: Padding(
+        padding: context.midSpacerOnlyHorizontal,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 72,
+              width: 72,
+              child: NormalNetworkImage(
+                  source: "https://wallpapercave.com/wp/wp2752752.jpg"),
+            ),
+            Gap(context.midSpacerSize),
+            const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                LabelText(
+                  text: "Ödüller",
+                  fontSize: FONT_SIZE.TITLE_MEDIUM,
+                  textColor: APPLICATION_COLOR.TITLE,
+                ),
+                LabelText(
+                  text: "Lorem ipsum dolor sit amet",
+                  fontSize: FONT_SIZE.TITLE_SMALL,
+                  textColor: APPLICATION_COLOR.SUBTITLE,
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
