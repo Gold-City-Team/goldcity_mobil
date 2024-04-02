0f56c05029038a3e52b1ad31dc07b43cf6765a66
Zafer Çetin <zaferkurumsal@gmail.com>
2024-04-02 17:23:58 +0300

    fast

--- lib/data/repository/project/project_repository_impl.dart
+++ lib/data/repository/project/project_repository_impl.dart
@@ -1,9 +1,12 @@
+import 'package:goldcity/data/source/remote/project/project_remote_data_source.dart';
 import 'package:goldcity/domain/entity/project/project_detail/project_detail_entity.dart';
 import 'package:goldcity/domain/repository/project/project_repository.dart';
+import 'package:goldcity/injection_container.dart';
 
 class ProjectRepositoryImpl implements ProjectRepository {
   @override
-  Future<ProjectDetailEntity> getDetail(int id) {
-    return 
+  Future<ProjectDetailEntity> getDetail(int id) async {
+    var result = await locator<ProjectRemoteDataSource>().getDetail(id);
+    return result.toEntity();
   }
 }
