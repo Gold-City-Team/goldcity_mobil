859b3368c4e74bab638b93f485e25380dc2e1ab5
Zafer Çetin <zaferkurumsal@gmail.com>
2024-05-27 15:00:29 +0300

    fast

--- lib/view/presentation/project/template/general_information_and_gallery_template/view/general_information_and_gallery_template_view.dart
+++ lib/view/presentation/project/template/general_information_and_gallery_template/view/general_information_and_gallery_template_view.dart
@@ -35,119 +35,125 @@ class GeneralInformationAndGalleryTemplateView extends StatelessWidget {
 
   Widget phoneView(BuildContext context,
       GeneralInformationAndGalleryTemplateViewModel value) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Stack(
-          children: [
-            Observer(builder: (context) {
-              if (value.templateTwo == null) {
-                return const SizedBox.shrink();
-              }
-              return SizedBox(
+    return SafeArea(
+      top: false,
+      right: false,
+      left: false,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Stack(
+            children: [
+              Observer(builder: (context) {
+                if (value.templateTwo == null) {
+                  return const SizedBox.shrink();
+                }
+                return SizedBox(
+                  width: context.sWidth,
+                  height: context.sWidth / 1.7777,
+                  child: NormalNetworkImage(
+                      source: value.templateTwo!.mediaItem.url,
+                      fit: BoxFit.cover),
+                );
+              }),
+              Container(
                 width: context.sWidth,
                 height: context.sWidth / 1.7777,
-                child: NormalNetworkImage(
-                    source: value.templateTwo!.mediaItem.url,
-                    fit: BoxFit.cover),
-              );
-            }),
-            Container(
-              width: context.sWidth,
-              height: context.sWidth / 1.7777,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    colors: [
-                      context.toColor(APPLICATION_COLOR.BACKGROUND_COLOR),
-                      Colors.transparent,
-                    ],
-                    begin: Alignment.bottomCenter,
-                    end: Alignment.topCenter,
-                    tileMode: TileMode.clamp),
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      colors: [
+                        context.toColor(APPLICATION_COLOR.BACKGROUND_COLOR),
+                        Colors.transparent,
+                      ],
+                      begin: Alignment.bottomCenter,
+                      end: Alignment.topCenter,
+                      tileMode: TileMode.clamp),
+                ),
               ),
-            ),
-          ],
-        ).animate().fade(),
-        Gap(context.largeSpacerSize),
-        Observer(builder: (context) {
-          if (value.templateTwo == null) {
-            return const SizedBox.shrink();
-          }
-          return Padding(
-            padding: context.largeSpacerOnlyHorizontal,
-            child: LabelText(
-              text: value.templateTwo!.title,
-              fontSize: FONT_SIZE.HEADLINE_LARGE,
-            ),
-          ).animate().fade();
-        }),
-        Gap(context.midSpacerSize),
-        Observer(builder: (context) {
-          if (value.templateTwo == null) {
-            return const SizedBox.shrink();
-          }
-          return Padding(
-            padding: context.largeSpacerOnlyHorizontal,
-            child: LabelText(
-              text: value.templateTwo!.subTitle,
-              fontSize: FONT_SIZE.TITLE_LARGE,
-              textColor: APPLICATION_COLOR.TITLE,
-            ),
-          ).animate().fade();
-        }),
-        Gap(context.largeSpacerSize),
-        Observer(builder: (context) {
-          if (value.templateTwo == null) {
-            return const SizedBox.shrink();
-          }
-          return Padding(
-            padding: context.largeSpacerOnlyHorizontal,
-            child: LabelText(
-              text: value.templateTwo!.description,
-              fontSize: FONT_SIZE.LABEL_LARGE,
-              textColor: APPLICATION_COLOR.SUBTITLE,
-            ),
-          ).animate().fade();
-        }),
-        Gap(context.largeSpacerSize),
-        Observer(builder: (context) {
-          if (value.templateTwo == null) {
-            return const SizedBox.shrink();
-          }
-          return Center(
-            child: Wrap(
-              alignment: WrapAlignment.center,
-              spacing: 10,
-              runSpacing: 0,
-              children: value.templateTwo!.gallery
-                  .map((e) => GestureDetector(
-                        onTap: () => value.navigateGallery(
-                          value.templateTwo!.gallery
-                              .indexWhere((element) => element == e),
-                        ),
-                        child: Padding(
-                          padding: context.midSpacerOnlyBottom,
-                          child: SizedBox(
-                            width: (context.sWidth / 2) - 20,
-                            height: ((context.sWidth / 2) - 20) / 1.7777,
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(8.0),
-                              child: Container(
-                                color: context.toColor(APPLICATION_COLOR.DARK),
-                                child: NormalNetworkImage(
-                                  fit: BoxFit.cover,
-                                  source: e.mediaItem.url,
-                                ),
-                              ),
-                            ).animate().fade(),
+            ],
+          ).animate().fade(),
+          Gap(context.largeSpacerSize),
+          Observer(builder: (context) {
+            if (value.templateTwo == null) {
+              return const SizedBox.shrink();
+            }
+            return Padding(
+              padding: context.largeSpacerOnlyHorizontal,
+              child: LabelText(
+                text: value.templateTwo!.title,
+                fontSize: FONT_SIZE.HEADLINE_LARGE,
+              ),
+            ).animate().fade();
+          }),
+          Gap(context.midSpacerSize),
+          Observer(builder: (context) {
+            if (value.templateTwo == null) {
+              return const SizedBox.shrink();
+            }
+            return Padding(
+              padding: context.largeSpacerOnlyHorizontal,
+              child: LabelText(
+                text: value.templateTwo!.subTitle,
+                fontSize: FONT_SIZE.TITLE_LARGE,
+                textColor: APPLICATION_COLOR.TITLE,
+              ),
+            ).animate().fade();
+          }),
+          Gap(context.largeSpacerSize),
+          Observer(builder: (context) {
+            if (value.templateTwo == null) {
+              return const SizedBox.shrink();
+            }
+            return Padding(
+              padding: context.largeSpacerOnlyHorizontal,
+              child: LabelText(
+                text: value.templateTwo!.description,
+                fontSize: FONT_SIZE.LABEL_LARGE,
+                textColor: APPLICATION_COLOR.SUBTITLE,
+              ),
+            ).animate().fade();
+          }),
+          Gap(context.largeSpacerSize),
+          Observer(builder: (context) {
+            if (value.templateTwo == null) {
+              return const SizedBox.shrink();
+            }
+            return Center(
+              child: Wrap(
+                alignment: WrapAlignment.center,
+                spacing: 10,
+                runSpacing: 0,
+                children: value.templateTwo!.gallery
+                    .map((e) => GestureDetector(
+                          onTap: () => value.navigateGallery(
+                            value.templateTwo!.gallery
+                                .indexWhere((element) => element == e),
                           ),
-                        ),
-                      ))
-                  .toList(),
-            ),
-          );
-        })
-      ],
+                          child: Padding(
+                            padding: context.midSpacerOnlyBottom,
+                            child: SizedBox(
+                              width: (context.sWidth / 2) - 20,
+                              height: ((context.sWidth / 2) - 20) / 1.7777,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(8.0),
+                                child: Container(
+                                  color:
+                                      context.toColor(APPLICATION_COLOR.DARK),
+                                  child: NormalNetworkImage(
+                                    fit: BoxFit.cover,
+                                    source: e.mediaItem.url,
+                                  ),
+                                ),
+                              ).animate().fade(),
+                            ),
+                          ),
+                        ))
+                    .toList(),
+              ),
+            );
+          })
+        ],
+      ),
     );
   }
 
