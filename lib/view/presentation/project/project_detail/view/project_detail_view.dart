f1322f405508021d2a4463129913ca20fdc51e2b
Zafer Çetin <zaferkurumsal@gmail.com>
2024-05-29 14:49:25 +0300

    fast

--- lib/view/presentation/project/project_detail/view/project_detail_view.dart
+++ lib/view/presentation/project/project_detail/view/project_detail_view.dart
@@ -26,89 +26,97 @@ class ProjectDetailView extends StatelessWidget {
       },
       onPageBuilder: (BuildContext context, ProjectDetailViewModel value) =>
           Scaffold(
-        body: Observer(builder: (context) {
-          if (value.isPageSelectorVisible) {
-            return PageSelectorWidget(
-                pages:
-                    value.entity!.detail.template.map((e) => e.title).toList(),
-                selectedIndex: value.templateIndex,
-                newIndex: (newIndex) => value.changeIndex(newIndex));
-          }
-          return Stack(
-            alignment: Alignment.bottomCenter,
-            children: [
-              Observer(builder: (context) {
-                return switch (
-                    value.entity!.detail.template[value.templateIndex].type) {
-                  TEMPLATE.TEMPLATE_ONE => const PossibiltyTemplateView(),
-                  TEMPLATE.TEMPLATE_TWO =>
-                    const GeneralInformationAndGalleryTemplateView()
-                };
-              }),
-              SafeArea(
-                child: Padding(
-                  padding: context.midSpacerOnlyHorizontal,
-                  child: Observer(builder: (context) {
-                    if (value.entity == null) {
-                      return const SizedBox.shrink();
-                    }
-                    return Row(
-                      children: [
-                        value.templateIndex != 0
-                            ? GestureDetector(
-                                onTap: () =>
-                                    value.changeIndex(value.templateIndex - 1),
-                                child: Container(
-                                  width: 50,
-                                  height: 50,
-                                  decoration: BoxDecoration(
-                                      color: context
-                                          .toColor(APPLICATION_COLOR.GOLD),
-                                      borderRadius: context.midRadius),
-                                  child: const Icon(Icons.keyboard_arrow_left),
-                                ),
-                              )
-                            : const SizedBox.shrink(),
-                        const Spacer(),
-                        GestureDetector(
-                          onTap: () => value.togglePageSelector(),
-                          child: Container(
-                            width: 50,
-                            height: 50,
-                            decoration: BoxDecoration(
-                                color: context.toColor(APPLICATION_COLOR.GOLD),
-                                borderRadius: context.midRadius),
-                            child: const Icon(Icons.menu),
-                          ),
-                        ),
-                        value.templateIndex !=
-                                value.entity!.detail.template.length - 1
-                            ? Gap(context.midSpacerSize)
-                            : const SizedBox.shrink(),
-                        value.templateIndex !=
-                                value.entity!.detail.template.length - 1
-                            ? GestureDetector(
-                                onTap: () =>
-                                    value.changeIndex(value.templateIndex + 1),
-                                child: Container(
-                                  width: 50,
-                                  height: 50,
-                                  decoration: BoxDecoration(
-                                      color: context
-                                          .toColor(APPLICATION_COLOR.GOLD),
-                                      borderRadius: context.midRadius),
-                                  child: const Icon(Icons.keyboard_arrow_right),
-                                ),
-                              )
-                            : const SizedBox.shrink()
-                      ],
-                    );
-                  }),
-                ),
-              )
-            ],
-          );
-        }),
+        body: Observer(
+          builder: (context) {
+            if (value.isPageSelectorVisible) {
+              return PageSelectorWidget(
+                  pages: value.entity!.detail.template
+                      .map((e) => e.title)
+                      .toList(),
+                  selectedIndex: value.templateIndex,
+                  newIndex: (newIndex) => value.changeIndex(newIndex));
+            }
+            return Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                Observer(builder: (context) {
+                  return switch (
+                      value.entity!.detail.template[value.templateIndex].type) {
+                    TEMPLATE.TEMPLATE_ONE => const PossibiltyTemplateView(),
+                    TEMPLATE.TEMPLATE_TWO =>
+                      const GeneralInformationAndGalleryTemplateView()
+                  };
+                }),
+                SafeArea(
+                  child: Padding(
+                    padding: context.midSpacerOnlyHorizontal,
+                    child: Observer(
+                      builder: (context) {
+                        if (value.entity == null) {
+                          return const SizedBox.shrink();
+                        }
+                        return Row(
+                          children: [
+                            value.templateIndex != 0
+                                ? GestureDetector(
+                                    onTap: () => value
+                                        .changeIndex(value.templateIndex - 1),
+                                    child: Container(
+                                      width: 50,
+                                      height: 50,
+                                      decoration: BoxDecoration(
+                                          color: context
+                                              .toColor(APPLICATION_COLOR.GOLD),
+                                          borderRadius: context.midRadius),
+                                      child:
+                                          const Icon(Icons.keyboard_arrow_left),
+                                    ),
+                                  )
+                                : const SizedBox.shrink(),
+                            const Spacer(),
+                            GestureDetector(
+                              onTap: () => value.togglePageSelector(),
+                              child: Container(
+                                width: 50,
+                                height: 50,
+                                decoration: BoxDecoration(
+                                    color:
+                                        context.toColor(APPLICATION_COLOR.GOLD),
+                                    borderRadius: context.midRadius),
+                                child: const Icon(Icons.menu),
+                              ),
+                            ),
+                            value.templateIndex !=
+                                    value.entity!.detail.template.length - 1
+                                ? Gap(context.midSpacerSize)
+                                : const SizedBox.shrink(),
+                            value.templateIndex !=
+                                    value.entity!.detail.template.length - 1
+                                ? GestureDetector(
+                                    onTap: () => value
+                                        .changeIndex(value.templateIndex + 1),
+                                    child: Container(
+                                      width: 50,
+                                      height: 50,
+                                      decoration: BoxDecoration(
+                                          color: context
+                                              .toColor(APPLICATION_COLOR.GOLD),
+                                          borderRadius: context.midRadius),
+                                      child: const Icon(
+                                          Icons.keyboard_arrow_right),
+                                    ),
+                                  )
+                                : const SizedBox.shrink()
+                          ],
+                        );
+                      },
+                    ),
+                  ),
+                )
+              ],
+            );
+          },
+        ),
       ),
     );
   }
