667f596b967c2e5e06fc62796cfbdfe0de26da10
Zafer Çetin <zaferkurumsal@gmail.com>
2024-05-30 17:12:43 +0300

    fast

--- lib/main.dart
+++ lib/main.dart
@@ -60,7 +60,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Gold City',
+      title: 'GoldCity',
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
       locale: context.locale,
