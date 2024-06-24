62472861020129ab912f89a4acb1a0ab5b76ca20
zafer <zaferkurumsal@gmail.com>
2024-06-24 22:37:10 +0300

    fast

--- lib/view/presentation/main/home/widget/condominium_trailer_widget.dart
+++ lib/view/presentation/main/home/widget/condominium_trailer_widget.dart
@@ -8,6 +8,7 @@ import 'package:goldcity/domain/entity/complex/complex/complex_entity.dart';
 import 'package:goldcity/util/constant/general_enum.dart';
 import 'package:goldcity/util/extension/design_extension.dart';
 import 'package:goldcity/util/extension/theme_extension.dart';
+import 'package:goldcity/util/extension/util_extension.dart';
 import 'package:goldcity/view/widget/image/normal_network_image.dart';
 import 'package:goldcity/view/widget/text/label_text.dart';
 import 'package:video_player/video_player.dart';
@@ -39,6 +40,10 @@ class _CondominiumTrailerWidgetState extends State<CondominiumTrailerWidget> {
 
   @override
   Widget build(BuildContext context) {
+    return isTablet() ? tabletView() : phoneView();
+  }
+
+  Widget tabletView() {
     return SizedBox(
       width: context.sWidth,
       height: context.sHeight,
@@ -123,6 +128,115 @@ class _CondominiumTrailerWidgetState extends State<CondominiumTrailerWidget> {
               onTap: () => widget.onExploreTap(),
               child: Container(
                 width: context.sWidth / 4 - 10,
+                height: 60,
+                margin: context.xLargeSpacerOnlyBottom,
+                decoration: BoxDecoration(
+                  color: context.toColor(APPLICATION_COLOR.DARK).withAlpha(200),
+                ),
+                padding: context.largeSpacerOnlyHorizontal,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    LabelText(
+                      text: LocaleKeys.explore.tr(),
+                      fontSize: FONT_SIZE.TITLE_LARGE,
+                    ),
+                    const Icon(Icons.keyboard_arrow_right_outlined, size: 36)
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget phoneView() {
+    return SizedBox(
+      width: context.sWidth,
+      height: context.sHeight,
+      child: Stack(
+        children: [
+          Align(
+            alignment: Alignment.topRight,
+            child: SizedBox(
+              width: context.sWidth,
+              height: context.sHeight,
+              child: Stack(
+                alignment: Alignment.bottomLeft,
+                children: [
+                  SizedBox.expand(
+                    child: FittedBox(
+                      fit: BoxFit.cover,
+                      child: SizedBox(
+                          width:
+                              context.sHeight * _controller.value.aspectRatio,
+                          height: context.sHeight * 1,
+                          child: VideoPlayer(_controller)),
+                    ),
+                  ),
+                  Container(
+                    height: context.sHeight,
+                    width: (context.sWidth / 2),
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                        colors: [
+                          context.toColor(APPLICATION_COLOR.BACKGROUND_COLOR),
+                          context
+                              .toColor(APPLICATION_COLOR.BACKGROUND_COLOR)
+                              .withAlpha(200),
+                          context
+                              .toColor(APPLICATION_COLOR.BACKGROUND_COLOR)
+                              .withAlpha(100),
+                          context
+                              .toColor(APPLICATION_COLOR.BACKGROUND_COLOR)
+                              .withAlpha(0),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: context.xlargeSpacerOnlyHorizontal,
+            child: SizedBox(
+              width: context.sWidth / 2,
+              height: context.sHeight,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: context.sWidth / 4,
+                    child: NormalNetworkImage(
+                        source: widget.complexEntity.complexDetail.logo.url),
+                  ),
+                  Gap(context.midSpacerSize),
+                  LabelText(
+                    text: widget.complexEntity.complexDetail.title,
+                    fontSize: FONT_SIZE.DISPLAY_LARGE,
+                  ),
+                  Gap(context.midSpacerSize),
+                  const LabelText(
+                    text: "Lorem ipsum dolor sit amet.Lorem ipsum",
+                    fontSize: FONT_SIZE.TITLE_LARGE,
+                  ),
+                  Gap(context.largeSpacerSize),
+                ],
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: GestureDetector(
+              onTap: () => widget.onExploreTap(),
+              child: Container(
+                width: context.sWidth / 2,
                 height: 60,
                 margin: context.xLargeSpacerOnlyBottom,
                 decoration: BoxDecoration(
