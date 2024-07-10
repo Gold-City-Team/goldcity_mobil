3359f3b2412e6eb685a0422822db84b4555c3356
Zafer Çetin <zaferkurumsal@gmail.com>
2024-07-10 11:19:26 +0300

    fast

--- lib/util/constant/navigation_constant.dart
+++ lib/util/constant/navigation_constant.dart
@@ -7,7 +7,8 @@ class NavigationConstant {
   static const COMPLEX_DETAIL = 'complex_detail';
   static const STORY = 'story';
   static const SETTINGS = 'settings';
-  static const WEBINARS = 'webinars';
+  static const WEBINARS = 'webinar';
+  static const EDUCATION = 'education';
   static const WEBINAR_DETAIL = 'webinar_detail';
   static const LEAD_APPLY = 'lead_apply';
   static const GALLERY = 'gallery';
