c1f28859ab72f18b395da72defc92b66e1def616
Zafer Çetin <zaferkurumsal@gmail.com>
2024-08-09 13:54:10 +0300

    fast

--- script/publish.sh
+++ script/publish.sh
@@ -1,11 +1,13 @@
-ssh idev << EOF
-cd /var/www/zafercetin.dev
+cd ..
+flutter build web --release
+ssh root@167.86.75.254 << EOF
+cd /zafercetin
 rm -rf *
 exit
 EOF
-scp -r /Users/zafercetin/Documents/goldcity_mobil/build/web/ idev:/var/www/zafercetin.dev
-ssh idev << EOF
-cd /var/www/zafercetin.dev/web
+scp -r /Users/zafercetin/Documents/goldcity_mobil/build/web/ root@167.86.75.254:/zafercetin
+ssh root@167.86.75.254 << EOF
+cd /zafercetin/web
 mv * ../
 exit
 EOF
